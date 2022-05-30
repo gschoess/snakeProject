fbc5cfb5f7512b02cd0b78dcc71c84c12b583022
Stephan <stephan.kost@th-brandenburg.de>
2022-05-30 16:43:35 +0200

    Framework_v1 gemäß Klassendiagramm

--- sprites/Snake.py
+++ sprites/Snake.py
@@ -7,9 +7,9 @@ from sprites.SnakeElement import SnakeElement
 class Snake(pygame.sprite.Group):
     def __init__(self):
         pygame.sprite.Group.__init__(self)
-        self.add(SnakeElement(300, 300, 1, 0))  # initial Snake with three elements moving to the right
-        self.add(SnakeElement(290, 300, 1, 0))
-        self.add(SnakeElement(280, 300, 1, 0))
+        self.add(SnakeElement(300, 300, 1, 0, '../images/snake_tile.png', 10, 10, '../sounds/pain.wav'))  # initial Snake with three elements moving to the right
+        self.add(SnakeElement(290, 300, 1, 0, '../images/snake_tile.png', 10, 10, '../sounds/pain.wav'))
+        self.add(SnakeElement(280, 300, 1, 0, '../images/snake_tile.png', 10, 10, '../sounds/pain.wav'))
 
     def update(self):
         pass
--- sprites/SpriteElement.py
+++ sprites/SpriteElement.py
@@ -6,15 +6,17 @@ from pygame.sprite import Sprite
 
 class SpriteElement(pygame.sprite.Sprite):
 
-    def __init__(self, x, y, dir_x, dir_y, image, sound):
+    def __init__(self, x, y, dir_x, dir_y, image, scale_x, scale_y, sound):
         Sprite.__init__(self)
         self.sound = sound
-        self.image = image
+        self.image = pygame.image.load(image)
+        self.image = pygame.transform.scale(self.image, (scale_x, scale_y))
         self.rect = self.image.get_rect()
         self.rect.left = x
         self.rect.top = y
         self.dir_x = dir_x
         self.dir_y = dir_y
+        self.sound = pygame.mixer.Sound(sound)
 
     def update(self):
         self.rect.left += self.dir_x
